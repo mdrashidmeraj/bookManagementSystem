4b01f62195f78c1622ccd89dbd3f03becdf93529
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2023-05-30 17:16:38 +0530

    book Store

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:@2002#Rashid@tcp(localhost:3306)/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDb() *gorm.DB {
+	return db
+}
